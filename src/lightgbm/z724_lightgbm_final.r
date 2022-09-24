b12e13e2da2bb9f078eefcf9502adbc6900fdc3d
vyago <vidalyago.94@gmail.com>
2022-09-24 09:12:06 -0300

    ka7232 google cloud

--- src/lightgbm/z724_lightgbm_final.r
+++ src/lightgbm/z724_lightgbm_final.r
@@ -16,18 +16,18 @@ require("lightgbm")
 #defino los parametros de la corrida, en una lista, la variable global  PARAM
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
-PARAM$experimento  <- "KA7231"
+PARAM$experimento  <- "KA7232"
 
 PARAM$input$dataset       <- "./datasets/competencia2_2022.csv.gz"
 PARAM$input$training      <- c( 202103 )
 PARAM$input$future        <- c( 202105 )
 
 PARAM$finalmodel$max_bin           <-     31
-PARAM$finalmodel$learning_rate     <-      0.024673   #0.0142501265
-PARAM$finalmodel$num_iterations    <-    343  #615
-#PARAM$finalmodel$num_leaves        <-   1015  #784
-PARAM$finalmodel$min_data_in_leaf  <-   5225  #5628
-PARAM$finalmodel$feature_fraction  <-   0.5889  #0.8382482539
+PARAM$finalmodel$learning_rate     <-      0.0072598   #0.0142501265
+PARAM$finalmodel$num_iterations    <-    1656  #615
+PARAM$finalmodel$num_leaves        <-   794  #784
+PARAM$finalmodel$min_data_in_leaf  <-   1315  #5628
+PARAM$finalmodel$feature_fraction  <-   0.247873  #0.8382482539
 PARAM$finalmodel$semilla           <- 102191
 
 #------------------------------------------------------------------------------
